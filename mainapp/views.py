2aa338c3ec78c151e1c2d0fc8346d878b04ee3b1
Yakim <alex.yakim.82@gmail.com>
2021-07-23 00:41:59 +0500

    menu.html and includes

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from .models import Menu, ComboMenu
+from .models import Menu, ComboMenu, InstragramPhoto, Testimonials
 
 
 # Create your views here.
+def instragram():
+    return InstragramPhoto.objects.all()
+
+
+def testimonials():
+    return Testimonials.objects.all()
+
 
 class MenuList(ListView):
     model = Menu
@@ -15,4 +22,12 @@ class MenuList(ListView):
 def menu_list(request):
     menu = Menu.objects.all()
     combo_menu = ComboMenu.objects.all()
-    return render(request, 'menu/menu.html', context={'menu_list': menu, 'combo_menu': combo_menu})
+    inst = instragram()
+    testim = testimonials()
+    context = {
+        'menu_list': menu,
+        'combo_menu': combo_menu,
+        'inst_photo': inst,
+        'testimonials': testim,
+    }
+    return render(request, 'menu/menu.html', context=context)
